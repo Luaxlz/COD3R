64dc7d6f1d02152ae79a7d9e6d41183a0043c5bd
Lucas Angeli <jl.angeli@outlook.com>
2022-06-20 18:11:20 -0300

    Fundamento JS

---
+++ exercicios-web/exercicios-JS/Fundamentos JS/hoisting.js
@@ -0,0 +1,12 @@
+console.log("a =", a);
+var a = 2;
+console.log("a =", a);
+
+
+// O que o compilador fez:
+// var a;
+// console.log("a =", a);
+// a = 2;
+// console.log("a =", a);
+
+//O efeito de issamento não ocorre quando usado com LET

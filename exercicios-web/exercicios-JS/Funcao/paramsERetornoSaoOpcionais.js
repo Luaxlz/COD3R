07ec5b35b57aaef160494addd5c35b3a8874b157
Lucas Angeli <jl.angeli@outlook.com>
2022-06-28 18:00:28 -0300

    funções

---
+++ exercicios-web/exercicios-JS/Funcao/paramsERetornoSaoOpcionais.js
@@ -0,0 +1,16 @@
+// Esse tipo de construção é válida porém pode gerar muitos problemas e bugs por isso o uso é desencorajado
+
+function area (largura, altura) {
+    const area = largura * altura
+    if (area > 20) {
+        console.log(`Valor acima do permitido: ${area}m2.`)
+    } else {
+        return area
+    }
+}
+
+console.log(area(2, 2))
+console.log(area(2))
+console.log(area())
+console.log(area(2, 3, 17, 22, 44))
+console.log(area(5, 5))

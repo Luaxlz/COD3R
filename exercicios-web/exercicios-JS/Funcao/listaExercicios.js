61439c5abe16a10c5c2bb1d376d27d74230edb30
Lucas Angeli <jl.angeli@outlook.com>
2022-07-09 13:53:31 -0300

    Update listaExercicios.js

--- exercicios-web/exercicios-JS/Funcao/listaExercicios.js
+++ exercicios-web/exercicios-JS/Funcao/listaExercicios.js
@@ -79,3 +79,117 @@
 //     return console.log(`O montante da aplicação financeira sob o regime de juros compostos é de R$${montante.toFixed(2)}.`)
 // }
 // investComposto(100, 0.1, 5)
+
+// 07) Uma das vantagens da programação é a automatização de tarefas que não gostamos de realizar. Dito isto,
+// elabore uma função cujo objetivo é resolver a fórmula de Bhaskara. Para isso, sua função deve receber três
+// parâmetros, “ax2”, “bx” e “c”, de tal modo que na equação: 3x² - 5x + 12 os valores seriam respectivamente: 3,
+// -5, 12. Como retorno deve ser passado um vetor que tem 2 valores um para cada possível resultado, mesmo
+// que os resultados sejam iguais. Caso o delta seja negativo, retorne, ao invés do vetor, um string com a frase:
+// “Delta é negativo”.
+// const bhaskara = (ax2, bx, c) => {
+//     let delta = (Math.pow(bx, 2) - (4 * ax2 * c))
+//     let x1
+//     let x2
+//     let resultado = []
+//     if (delta < 0) {
+//         console.log("Delta é negativo.")
+//     } else if (delta === 0) {
+//         x1 = ((-bx) + (Math.sqrt(delta))) / 2 * ax2
+//         resultado = [x1, x1]
+//         console.log(resultado)
+
+//     } else {
+//         x1 = ((-bx) + (Math.sqrt(delta))) / (2 * ax2)
+//         x2 = ((-bx) - (Math.sqrt(delta))) / (2 * ax2)
+//         resultado.push(x1.toFixed(2), x2.toFixed(2))
+//         console.log(resultado)
+//     }
+// }
+
+// bhaskara(3, -5, 12)
+
+// 08) Pedro joga N jogos de basquete por temporada. Para saber como está ele está progredindo, ele mantém
+// registro de todos os as pontuações feitas por jogo. Após cada jogo ele anota no novo valor e confere se o
+// mesmo é maior ou menor que seu melhor e pior desempenho. Dada uma lista string = “pontuação1 pontuação2
+// pontuação3 etc..”, escreva uma função que ao recebê-la irá comparar os valores um a um e irá retornar um
+// vetor com o número de vezes que ele bateu seu recorde de maior número de pontos e quando fez seu pior
+// jogo. (Número do pior jogo).
+// let stringPontuacoes = "30, 40, 20, 4, 51, 25, 42, 38, 56, 0"
+
+// const desempenhoJogos = (stringPontuacoes) => {
+//     let pontuacao = stringPontuacoes.split(", ")
+//     let melhorPonto = pontuacao[0]
+//     let piorPonto = pontuacao[0]
+//     let vezesRecorde = 0
+//     let piorJogo = 0
+//     for(let i = 1; i < pontuacao.length; i++) {
+//             if (pontuacao[i] > melhorPonto) {
+//                 melhorPonto = pontuacao[i]
+//                 vezesRecorde++
+//             } else if(pontuacao[i] < piorPonto) {
+//                 piorPonto = pontuacao[i]
+//                 piorJogo = i + 1
+//             }
+//         }
+//     return [vezesRecorde, piorJogo]
+// }
+
+// console.log(desempenhoJogos(stringPontuacoes))
+
+// 09) Construa uma função para um sistema de notas de uma instituição que possui a seguinte política de
+// classificação: Todo aluno recebe uma nota de 0 a 100. Alunos com nota abaixo de 40 são reprovados. As notas
+// possuem a seguinte regra de arredondamento: Se a diferença entre a nota e o próximo múltiplo de 5 for menor
+// que 3, arredondar a nota para esse próximo múltiplo de 5. Se a nota for abaixo de 38, não é feito nenhum
+// arredondamento pois esta nota resulta na reprovação do aluno. Por exemplo, a nota 84 será arredondada para
+// 85, mas a nota 29 não será arredondada por ser abaixo de 40 e não ser possível arredondamento eficiente, ou
+// seja, que evite a reprovação do aluno. No caso de a nota ser 38, o arredondamento é possível pois atingirá 40
+// e o aluno será aprovado.
+// const sistemaNotas = (nota) => {
+//     if(nota > 100 || nota < 0) {
+//         console.log(`Nota inválida.`)
+//     } else if(nota < 38) {
+//         console.log(`Aluno reprovado com nota ${nota}`)
+//     } else if( nota >= 98) {
+//         nota = 100
+//     } else if(nota >= 93 && nota < 95) {
+//         nota = 95
+//         console.log(`Aluno aprovado com nota ${nota}`)
+//     } else if (nota >= 88 && nota < 90) {
+//         nota = 90
+//         console.log(`Aluno aprovado com nota ${nota}`)
+//     } else if (nota >= 83 && nota < 85) {
+//         nota = 85
+//         console.log(`Aluno aprovado com nota ${nota}`)
+//     } else if (nota >= 78 && nota < 80) {
+//         nota = 80
+//         console.log(`Aluno aprovado com nota ${nota}`)
+//     } else if (nota >= 73 && nota < 75) {
+//         nota = 75
+//         console.log(`Aluno aprovado com nota ${nota}`)
+//     } else if (nota >= 68 && nota < 70) {
+//         nota = 70
+//         console.log(`Aluno aprovado com nota ${nota}`)
+//     } else if (nota >= 63 && nota < 65) {
+//         nota = 65
+//         console.log(`Aluno aprovado com nota ${nota}`)
+//     } else if (nota >= 58 && nota < 60) {
+//         nota = 60
+//         console.log(`Aluno aprovado com nota ${nota}`)
+//     } else if (nota >= 53 && nota < 55) {
+//         nota = 55
+//         console.log(`Aluno aprovado com nota ${nota}`)
+//     } else if (nota >= 48 && nota < 50) {
+//         nota = 50
+//         console.log(`Aluno aprovado com nota ${nota}`)
+//     } else if (nota >= 43 && nota < 45) {
+//         nota = 45
+//         console.log(`Aluno aprovado com nota ${nota}`)
+//     } else if (nota >= 38 && nota < 40) {
+//         nota = 40
+//         console.log(`Aluno aprovado com nota ${nota}`)
+//     } else {
+//         console.log(`Aluno aprovado com nota ${nota}`)
+//     }
+// }
+
+// sistemaNotas(38)

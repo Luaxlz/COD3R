8097fbd8534c66fb849a8f1a9e5683bcbe665713
Lucas Angeli <jl.angeli@outlook.com>
2022-07-28 17:48:35 -0300

    Update exercicios.js

--- exercicios-web/exercicios-JS/Lista-Exercicios/exercicios.js
+++ exercicios-web/exercicios-JS/Lista-Exercicios/exercicios.js
@@ -10,3 +10,57 @@
 //     return `Idade em dias: ${idadeEmdias.toFixed(2)}!`
 // }
 // console.log(anosParaDias(10))
+
+// Desenvolva uma função que recebe dois parâmetros, um é a quantidade de horas trabalhadas por um funcionário num mês, e o quanto ele recebe por hora. O retorno da função deve ser a string "Salário igual a R$ Х", em que Х é o quanto o funcionário ganhou no mês.
+// const calculaSalario = (horasTrabalhadas, salarioHora) => {
+//     console.log(`Salário igual a R$ ${(horasTrabalhadas * salarioHora).toFixed(2)}`)
+// }
+// calculaSalario(150, 40.5)
+
+// Crie uma função que recebe um número (de 1 a 12) e retorne o mês correspondente como uma string. Por exemplo, se a entrada for 2, a função deverá retornar "fevereiro", pois este é o 2° mês.
+// const retornaMes = num => {
+//     switch(num) {
+//         case 1: case 01:
+//             console.log('Janeiro')
+//             break
+//         case 2: case 02:
+//             console.log('Fevereiro')
+//             break
+//         case 3: case 03:
+//             console.log('Março')
+//             break
+//         case 4: case 04:
+//             console.log('Abril')
+//             break
+//         case 5: case 05:
+//             console.log('Maio')
+//             break
+//         case 6: case 06:
+//             console.log('Junho')
+//             break
+//         case 7: case 07:
+//             console.log('Julho')
+//             break
+//         case 8: case 08:
+//             console.log('Agosto')
+//             break
+//         case 9: case 09:
+//             console.log('Setembro')
+//             break
+//         case 10:
+//             console.log('Outubro')
+//             break
+//         case 11:
+//             console.log('Novembro')
+//             break
+//         case 12:
+//             console.log('Dezembro')
+//             break
+//         default:
+//             console.log('Dado Inválido')
+//             break
+//     }
+// }
+
+// retornaMes(111)
+

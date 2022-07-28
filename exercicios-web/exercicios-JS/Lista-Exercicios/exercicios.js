7a3348b2dd781788ddcc071f13d8b6bcf30ea640
Lucas Angeli <jl.angeli@outlook.com>
2022-07-27 21:01:53 -0300

    update arrays e exercicios

---
+++ exercicios-web/exercicios-JS/Lista-Exercicios/exercicios.js
@@ -0,0 +1,12 @@
+// ; Crie uma função que retorna a string "Olá, " concatenada com um argumento text (a ser passado para a função) e com ponto de exclamação "!" no final.
+// function cumprimentar(texto) {
+//     console.log(`Olá, ${texto}!`)
+// }
+// cumprimentar('me chamo Leopoldino')
+
+// Escreva uma função que receba a idade de uma pessoa em anos e retorne a mesma idade em dias.
+// const anosParaDias = idade => {
+//     let idadeEmdias = idade * 365
+//     return `Idade em dias: ${idadeEmdias.toFixed(2)}!`
+// }
+// console.log(anosParaDias(10))

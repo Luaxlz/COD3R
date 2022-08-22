bb131bbc31d1a3a72ad59fae3ba25ee8c6b3bcb9
Lucas Angeli <jl.angeli@outlook.com>
2022-08-22 18:09:54 -0300

    eventos e plugins

---
+++ exercicios-web/jquery/js/temporizador.js
@@ -0,0 +1,11 @@
+(function ($) {
+    $.fn.temporizador = function (opcoes) {
+        const opcoesFinais = $.extend({
+            mensagem: 'Em breve!',
+            horario: '23:59:59'
+        }, opcoes)
+
+        cosnt horaDezena = $('<span> class="digito">').html('0')
+        return this
+    }
+})(jQuery)

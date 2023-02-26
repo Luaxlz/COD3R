0fa567cf73dc3781bbb82a255228cd3a789e2629
Lucas Angeli <jl.angeli@outlook.com>
2023-02-26 13:09:15 -0300

    Gulp

--- exercicios-web/gulp/javascript/gulpfile.js
+++ exercicios-web/gulp/javascript/gulpfile.js
@@ -4,7 +4,18 @@ const concat = require('gulp-concat')
 const uglify = require('gulp-uglify')
 const babel = require('gulp-babel')
 
-function default(cb) {
-    
+function padrao(cb) {
+    gulp.src('src/**/*.js')
+        .pipe(babel({
+            comments: false,
+            presets: ["env"]
+        }))
+        .pipe(uglify())
+        .on('error', err => console.log(err))
+        .pipe(concat('codigo.min.js'))
+        .pipe(gulp.dest('build'))
+
     return cb()
 }
+
+exports.default = series(padrao)

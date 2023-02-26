3772763bc291907ace38e014eadf056d7cec4ba7
Lucas Angeli <jl.angeli@outlook.com>
2023-02-26 10:05:45 -0300

    Update gulpfile.js

---
+++ exercicios-web/gulp/javascript/gulpfile.js
@@ -0,0 +1,10 @@
+const { series } = require('gulp')
+const gulp = require('gulp')
+const concat = require('gulp-concat')
+const uglify = require('gulp-uglify')
+const babel = require('gulp-babel')
+
+function default(cb) {
+    
+    return cb()
+}

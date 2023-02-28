5499e5c714ffc720257d869bc2bcac5261335ba3
Lucas Angeli <jl.angeli@outlook.com>
2023-02-28 13:12:53 -0300

    Webpack

--- exercicios-web/webpack/webpack.config.js
+++ exercicios-web/webpack/webpack.config.js
@@ -1,6 +1,27 @@
 const webpack = require('webpack')
+const miniCssExtractPlugin = require("mini-css-extract-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-    mode: 'development',
-    entry: './src/principal.js'
+    mode: "development",
+    entry: './src/principal.js',
+    output: {
+        filename: 'principal.js',
+        path: __dirname + '/public'
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "estilo.css"
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+                miniCssExtractPlugin.loader,
+                //'style-loader', //Adiciona CSS dentro da DOM injetando a tag <style>
+                'css-loader' // interpreta @import, url()...
+            ]
+        }]
+    }
 }
